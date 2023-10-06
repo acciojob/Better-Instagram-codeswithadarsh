8e2e3e08e4dc4d20c1ea55564782571e70e1a7f2
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-06 11:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-// Get all pic elements
-var pics = document.querySelectorAll(".pic");
+//your code here
 
-// Add drag and drop event listeners to each pic element
-pics.forEach(function(pic) {
-	pic.addEventListener("dragstart", dragStart);
-	pic.addEventListener("dragover", dragOver);
-	pic.addEventListener("dragenter", dragEnter);
-	pic.addEventListener("dragleave", dragLeave);
-	pic.addEventListener("drop", drop);
-	pic.addEventListener("dragend", dragEnd);
-});
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
 
-// Global variables for drag and drop operations
-var sourcePic = null;
-var targetPic = null;
+const images = document.querySelectorAll(".image");
 
-// Drag and drop event functions
-function dragStart(event) {
-	sourcePic = event.target;
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
 
-function dragOver(event) {
-	event.preventDefault();
+function allowDrop(e) {
+  e.preventDefault();
 }
 
-function dragEnter(event) {
-	targetPic = event.target;
-	targetPic.classList.add("highlight");
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
+
+  dragdrop(clone);
+
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
 
-function dragLeave(event) {
-	targetPic = event.target;
-	targetPic.classList.remove("highlight");
-}
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
 
-function drop(event) {
-	event.preventDefault();
-	targetPic = event.target;
-
-	// Swap background images of source and target pics
-	var sourceBG = sourcePic.style.backgroundImage;
-	sourcePic.style.backgroundImage = targetPic.style.backgroundImage;
-	targetPic.style.backgroundImage = sourceBG;
-}
-
-function dragEnd(event) {
-	// Remove highlight from target pic
-	targetPic = event.target;
-	targetPic.classList.remove("highlight");
-}
+images.forEach(dragdrop);
